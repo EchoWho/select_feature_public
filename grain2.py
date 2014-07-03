ba60795ec75417c93a962f5b916b5e292842a897
Hanzhang Hu <echo.hhz.nj@gmail.com>
2014-07-03 18:55:25 -0400

    grain2

---
+++ grain2.py
@@ -0,0 +1,25 @@
+import numpy as np
+import sys
+import dataset2
+import opt2
+
+def doit():
+
+  D = dataset2.Dataset('grain_data', 'grain_results', 'grain.groups.npz')
+  fn_trains = 'toy.train.txt' #sys.argv[1] #e.g. 
+  fn_test = 'toy.valid.txt' #sys.argv[2] #e.g. 
+  params={}
+  params['l2_lam'] = 1e-6
+  params['regression_methods'] = ['glm']
+  params['opt_methods'] = ['OMP']
+  params['glm_power'] = 5
+  params['glm_max_iter'] = 10
+  params['classification'] = True
+  params['val_map'] = lambda x: x - 1
+
+  D.train(fn_trains, params)
+  return D.evaluate_multi_files(fn_test, fn_trains, params)
+    
+
+if __name__ == "__main__":
+  doit()
