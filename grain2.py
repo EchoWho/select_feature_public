c661c144018de7c36ce2637c08d1518e8c7cca82
EchoWho <hanzhang@andrew.cmu.edu>
2014-07-07 17:05:50 -0400

    grain2 experiments

--- grain2.py
+++ grain2.py
@@ -6,11 +6,11 @@ import opt2
 def doit():
 
   D = dataset2.Dataset('grain_data', 'grain_results', 'grain.groups.npz')
-  fn_trains = 'toy.train.txt' #sys.argv[1] #e.g. 
-  fn_test = 'toy.valid.txt' #sys.argv[2] #e.g. 
+  fn_trains = 'fold1.train.txt' #sys.argv[1] #e.g. 
+  fn_test = 'fold1.test.txt' #sys.argv[2] #e.g. 
   params={}
   params['l2_lam'] = 1e-6
-  params['regression_methods'] = ['glm']
+  params['regression_methods'] = ['linear','logistic', 'glm']
   params['opt_methods'] = ['OMP']
   params['glm_power'] = 5
   params['glm_max_iter'] = 10
