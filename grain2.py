b933bcb1644da7b39acb0dd789f5466065c42084
EchoWho <hanzhang@andrew.cmu.edu>
2014-07-07 19:18:55 -0400

    temp

--- grain2.py
+++ grain2.py
@@ -7,7 +7,7 @@ def doit():
 
   D = dataset2.Dataset('grain_data', 'grain_results', 'grain.groups.npz')
   fn_trains = 'fold1.train.txt' #sys.argv[1] #e.g. 
-  fn_test = 'fold1.test.txt' #sys.argv[2] #e.g. 
+  fn_test = 'fold1.valid.txt' #sys.argv[2] #e.g. 
   params={}
   params['l2_lam'] = 1e-6
   params['regression_methods'] = ['linear','logistic', 'glm']
@@ -17,7 +17,7 @@ def doit():
   params['classification'] = True
   params['val_map'] = lambda x: x - 1
 
-  D.train(fn_trains, params)
+  #D.train(fn_trains, params)
   return D.evaluate_multi_files(fn_test, fn_trains, params)
     
 
