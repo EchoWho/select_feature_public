e1f25ce47e578c9e0bc0594f5e47918d3bb59eb8
Nick Rhinehart <nrhine1@gmail.com>
2014-08-01 19:00:25 -0400

    print fix

--- opt2.py
+++ opt2.py
@@ -103,7 +103,7 @@ def opt_glm_explicit(X, Y, potential_func, mean_func, w0=None,
       if has_converge:
         break
       if last_objective < objective:
-        gcp.info("iteration: {}. Step size was too large. Shrinking!!!")
+        gcp.info("iteration: {}. Step size was too large. Shrinking!!!".format(nbr_iter))
         #print "iteration: {}. Step size was too large. Shrinking!!!"
         total_delta_w *= beta
         w = last_w - total_delta_w
